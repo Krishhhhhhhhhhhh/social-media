333460d85d2c248010d0a6ba3b31e5d6c6ef2590
krishna <pathakrishna2704@gmail.com>
2025-09-30 17:31:16 +0530

    refactor: restructure serve function parameters for clarity

--- Backend/api/inngest.js
+++ Backend/api/inngest.js
@@ -1,3 +1,5 @@
 import {serve} from "inngest/express";
 import { inngest,functions} from "../inngest/index.js";
-export default serve(inngest, functions);
+export default serve({
+    client:inngest, 
+    functions});
